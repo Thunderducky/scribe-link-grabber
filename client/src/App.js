830043533f177d1b114d555266fa8dbe6f5f098a
Baggio Shehadi <k3nh1n@yahoo.com>
2019-03-27 13:50:41 -0500

    added some comments to explain the seconds to Hms function

--- client/src/App.js
+++ client/src/App.js
@@ -23,12 +23,33 @@ class App extends Component {
   }
   
  secondsToHms(d) {
+	 //makes sure its a number
     d = Number(d);
-
-    var h = Math.floor(d / 3600);
-    var m = Math.floor(d % 3600 / 60);
+	// gets the hours by dividing the seconds by 3600 and flooring it
+	//E.G. : 
+		//h = Math.floor(4000/3600)
+		//h = Math.floor(1.111111111111111)
+		//h = 1
+	var h = Math.floor(d / 3600);
+	//finds the minutes by getting the remainder of the hours then dividing it to get the minutes floors it so it doesn't have decimal spaces for the seconds
+	//E.G. 
+		//m = Math.floor(4000%3600/60)
+		//m = Math.floor(400/60)
+		//m = Math.floor(6.666666666666667)
+		//m = 6
+	var m = Math.floor(d % 3600 / 60);
+	//Finds the seconds by getting the remainder of the hours then minutes,
+	//E.G.
+		//h = Math.floor(4000 % 3600 % 60)
+		//h = Math.floor(400 % 60)
+		//s = Math.floor(40)
+		//s = 40
     var s = Math.floor(d % 3600 % 60);
-
+	//makes sure theres a 0 and if theres more than two number associated w/ the variable it will cut it to be the last two
+	//eg
+	//return "01" + 			 ":" + "06"				   + ":" + "040"
+	//in this case theres 3 numbers in the seconds so it takes the last two numbers which makes it 40
+	//return "01:06:40"
     return ("0" + h).slice(-2) + ":" + ("0" + m).slice(-2) + ":" + ("0" + s).slice(-2);
 }
   componentDidMount(){
